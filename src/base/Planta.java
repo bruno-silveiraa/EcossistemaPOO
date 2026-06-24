e8a907140a3ed1d2138f5070efab9dc9b3ce2a73
ChDan1el <danielchristianolima@gmail.com>
2026-06-24 15:37:11 -0300

    Limite de crescimento

--- src/base/Planta.java
+++ src/base/Planta.java
@@ -13,29 +13,34 @@ public class Planta extends SerVivo {
     }
 
     public void crescer() {
-        if (vivo) {
-            tamanho++;
-            ganharEnergia(5);
-            System.out.println(nome + " cresceu. Tamanho atual: " + tamanho);
+        if (!vivo) {
+            System.out.println("  " + nome + " esta morta e nao pode mais crescer.");
+            return;
         }
+        if (tamanho >= 10) {
+            System.out.println("  " + nome + " ja atingiu o tamanho maximo!");
+            return;
+        }
+        tamanho ++;
+        ganharEnergia(5);
+        System.out.println("  " + nome + " cresceu! Tamanho: " + tamanho + " (+5 de energia)");
     }
 
     public void serConsumida(int quantidade) {
-        if (vivo) {
-            perderEnergia(quantidade);
-            tamanho--;
-
-            if (tamanho < 0) {
-                tamanho = 0;
-            }
-
-            System.out.println(nome + " foi consumida e perdeu energia.");
+        if (!vivo) return;
+        perderEnergia(quantidade);
+        tamanho = Math.max(0, tamanho - 1);
+        if (energia <= 0) {
+            vivo = false;
+            System.out.println("  " + nome + " foi totalmente consumida e morreu.");
+        } else {
+            System.out.println("  " + nome + " foi consumida -> Energia restante: " + energia + " | Tamanho atual: "+tamanho);
         }
     }
 
     @Override
     public void mostrarEstado() {
         String estado = vivo ? "viva" : "morta";
-        System.out.println("Planta " + nome + " esta " + estado + ", tamanho " + tamanho + " e energia " + energia);
+        System.out.println("  [Planta] : " + nome + " | Estado: " + estado + " | Tamanho: " + tamanho + " | Energia: " + energia);
     }
 }
